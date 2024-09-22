568364448f765416b16233cbd1557dbdd180cbb8
techmannih <manishchaudharyttt@gmail.com>
2024-09-22 13:30:38 +0530

    add navbar and main section

---
+++ src/app/component/MainSection/MainSection.jsx
@@ -0,0 +1,17 @@
+export default function MainSection() {
+  return (
+    <div className="text-white text-center mt-10">
+      <h1 className="text-[170.92px] font-bold leading-[218.77px] tracking-[2%] text-center">
+        Voyex.
+      </h1>
+
+      <div className="flex justify-center">
+        <p className="mt-4 text-center opacity-88 font-normal text-[28.49px] leading-[38.46px] tracking-[2%] w-[1120px]">
+          Yorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
+          fringilla accumsan, risus sem.
+        </p>
+      </div>
+    </div>
+  );
+}
