1f732ee449f5c89c95a4f24b86975a3e015fa5e9
techmannih <manishchaudharyttt@gmail.com>
2024-10-01 20:46:46 +0530

    fix style for main section and research section

--- src/app/component/MainSection/MainSection.jsx
+++ src/app/component/MainSection/MainSection.jsx
@@ -4,21 +4,24 @@ export default function MainSection() {
   return (
     <div className="text-white text-center ">
       <div className="bg-[rgba(114,249,103,1)] h-[50px] flex items-center justify-center"></div>
-      <h1 className="text-[64px] font-bold leading-[81px] tracking-[2%] text-center w-[833px] mx-auto mt-10">
+      <h1 className="text-[64px] font-bold leading-[81.92px] tracking-[0.02em] text-center w-[833px] mx-auto mt-10">
         Voyex Lorem Ipsum Yorem ipsum dolor sit a
       </h1>
       <div className="flex justify-center">
-        <p className="mt-4 text-center opacity-88 font-normal text-[28.49px] leading-[38.46px] tracking-[2%] w-[1120px]">
+        <p className="mt-4 text-center opacity-88 font-normal text-[28.49px] leading-[36.46px] tracking-[0.02em] w-[1155px]">
           Yorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
           turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
           fringilla accumsan, risus sem.
         </p>
       </div>
       <div className="flex justify-center mt-8 gap-6">
-        <button className="mt-8 w-[208.45px] h-[38.91px] text-md bg-[rgba(70,186,60,1)]  rounded-full opacity-100 flex justify-center px-6 items-center">
-          <span className="text-black ">Start Your AI Journey</span>
-          {/* <IoArrowForwardOutline  className="text-[rgba(70,186,60,1)]" /> */}
+        <button className="mt-8 w-[208.45px] h-[38.91px] bg-[rgba(70,186,60,1)] rounded-full flex justify-center items-center opacity-100 px-6">
+          <span className="text-black text-[14px] font-medium leading-[17.92px]"  >
+            Start Your AI Journey
+          </span>
+          {/* <IoArrowForwardOutline className="text-[rgba(70,186,60,1)]" /> */}
         </button>
+
         <button className="mt-8 w-[208.45px] h-[38.91px] text-md  border-[1.48px] border-solid border-[rgba(70,186,60,1)]  rounded-full opacity-100 flex justify-center px-6 items-center">
           <span className="text-[rgba(70,186,60,1)] ">Upload Product</span>
           {/* <IoArrowForwardOutline  className="text-[rgba(70,186,60,1)]" /> */}
